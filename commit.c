6233af442a05f13620c4b1874cb7897fba622ca6
Adi <adityakumar1975in@gmail.com>
2026-04-17 23:43:23 +0530

    commit: attach current HEAD parent

--- commit.c
+++ commit.c
@@ -199,6 +199,12 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
 
     if (tree_from_index(&commit.tree) != 0) return -1;
 
+    if (head_read(&commit.parent) == 0) {
+        commit.has_parent = 1;
+    } else {
+        commit.has_parent = 0;
+    }
+
     (void)message; (void)commit_id_out;
     return -1;
 }
